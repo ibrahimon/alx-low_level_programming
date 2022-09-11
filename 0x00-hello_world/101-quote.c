fb95d1206f5a7d3303fa9181faab69433c6167fb
Ibrahimon <ibrahim07helal@gmail.com>
2022-09-11 10:41:55 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main: Print a quote
+ * Description: using printf
+ * Return: 1
+ **/
+
+int main(void)
+
+{
+	char quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+		write(2, ch, sizeof(ch));
+		return (1);
+}
