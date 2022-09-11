166fa6658b72e6b989234ca4eaeccc2550c1e7e7
Ibrahim Helal <54383232+ibrahimon@users.noreply.github.com>
2022-09-11 16:19:11 +0200

    bretty

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,21 +1,18 @@
 #include <stdio.h>
 
 
+
 /**
- *
- *  * main - Print a string
- *
- *   * Description: use printf
- *
- *    * Return: 0
- *
+ * main - Print a string
+ * Description: using printf
+ * Return: 0
  */
 
 int main()
+	
 {
+	printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
 
-		printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
-
-	 	       return (0);
+		 return (0);
 
 }
