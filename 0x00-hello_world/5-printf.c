0c0b86bccd4882544cb648b0a5bd925d4e1c26cd
Ibrahimon <ibrahim07helal@gmail.com>
2022-09-11 10:27:23 -0700

    all

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,16 +2,25 @@
 
 
 
+
+
+
+
 /**
- * main - Print a string
+ * main: Print a string
  * Description: using printf
  * Return: 0
- */
+ **/
 
-int main(void)
+
+int main(void)		
 	
 {
+
 printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
+
 return (0);
 
-}
+
+
+}}
