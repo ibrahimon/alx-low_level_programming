baca0299b7beb072a363b3247892238b495747b5
Ibrahimon <ibrahim07helal@gmail.com>
2022-09-12 08:44:11 -0700

    prints all possible combinations of single-digit numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ * Description: prints all possible combinations of single-digit numbers.
+ * Return: 0
+ */
+
+int main(void)
+{
+	int x = 0;
+
+	while (c < 10)
+	{
+		putchar(48 + x);
+
+		if (x != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		x++;
+	}
+
+	putchar('\n');
+	return (0);
+}
