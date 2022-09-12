dc297bc7c9caaf6f73143188bc1b136a8c63e382
Ibrahimon <ibrahim07helal@gmail.com>
2022-09-12 05:16:34 -0700

    alphabetz

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ * Description: Printingg the alphabet in lowercase
+ * Return: chars
+ */
+
+int main(void)
+
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchat(c);
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+}
