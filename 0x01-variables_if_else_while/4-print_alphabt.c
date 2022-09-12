b8d83d4a01734a690fafcd6f64c5cb32fd790694
Ibrahimon <ibrahim07helal@gmail.com>
2022-09-12 07:09:01 -0700

    print aphabtz except e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ *  main - main block
+ *  Desciption: Printing alphabet except e and q
+ *  Return: 0
+ */
+
+int main(void)
+{
+	char x = 'a';
+
+	while (x <= 'z')
+	{
+		if (x != 'e' && x != 'q')
+		{
+			putchar(x);
+		}
+		x++;
+	}
+	
+	putchar('\n');
+	return (0);
+}
