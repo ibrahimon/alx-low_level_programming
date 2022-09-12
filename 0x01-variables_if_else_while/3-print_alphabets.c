068f9697303b22e5f8a93d0782fe894ac23124aa
Ibrahimon <ibrahim07helal@gmail.com>
2022-09-12 05:29:14 -0700

    alphabetz lower and upper

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ * Descreption: printing alphabet in loxercase then in upper case
+ * Return: 0 (succeed)
+ */
+
+int main(void)
+
+{
+	char x = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+
+	c = 'A';
+	
+	while (c <= 'Z')
+	{
+		putchar(c);
+		c++;
+	}
+
+
+	putchar('\n);
+	return (0);
+}
