390290a600fd544d6fdc464b6f014825342f2bef
Ibrahim Helal <54383232+ibrahimon@users.noreply.github.com>
2022-09-14 00:51:03 +0200

    putcharing

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ * Descreption: Printing _putchar, follwed by new line
+ * Return: 0
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+
+	putchar('\n');
+	return (0);
+}
