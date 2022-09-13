3e3dcc7787e23d0dc38e5fa3a50103e15f4c7b22
Ibrahim Helal <54383232+ibrahimon@users.noreply.github.com>
2022-09-14 00:58:04 +0200

    putcharing

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <main.h>
+#include "main.h"
 
 /**
  * main - main block
@@ -17,6 +17,6 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 
-	putchar('\n');
+	_putchar('\n');
 	return (0);
 }
