53e56a7f8c1d18f29683de5491f1b83a5b28f303
Ibrahim Helal <54383232+ibrahimon@users.noreply.github.com>
2022-09-13 20:41:15 +0200

    printing _putchar

---
+++ 0x02-function_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ * Description: Printing  _putchar, followed by a new line.
+ * Return: 0 
+ */
+
+int main(void)
+{
+	printf("%S", "_putchar");
+	printf('\n');
+	return (0);
+}
+
