10509d8476bebeba6c619bc3394a8fab2080502f
Ibrahim Helal <54383232+ibrahimon@users.noreply.github.com>
2022-09-14 21:50:17 +0200

    function

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,40 +16,45 @@ void times_table(void)
 	while (a <= 9)
 	{
 		b = 0;
+		c = a * b;
 
 		while (b <= 9)
 		{
-			c = a * b;
 
-			if (b == 0)
+			if ((c / 10) == 0)
 			{
-				_putchar('0' + c);
-			}
+				if (b == 0)
+				{
+					_putchar('0');
+				}
 
-			else if (c < 10)
-			{
-				_putchar(' ');
-				_putchar('0' + c);
+				if (b != 0)
+				{
+					_putchar(' ');
+					_putchar((c % 10) + '0');
+				}
+
+				if (b < 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
 			}
 
 			else
 			{
-				_putchar('0' + (c / 10));
-				_putchar('0' + (c % 10));
+			       	_putchar((c / 10) + '0');
+				_putchar((c % 10) + '0');
+
+				if (b < 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
 			}
-
 		}
-		  if (b < 9)
-		  {
-			  _putchar(',');
-			  _putchar(' ');
-		  }
-
-		  b++;
-
+		
+		_putchar('\n');
 	}
-
-	_putchar('\n');
-	a++;
 
 }
