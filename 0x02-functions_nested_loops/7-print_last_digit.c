be96ad7fd2cb100f0c93999f17662841c54092a5
Ibrahim Helal <54383232+ibrahimon@users.noreply.github.com>
2022-09-14 20:45:39 +0200

    function

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,19 +12,22 @@ int print_last_digit(int x)
 {
 	int last_d;
 
+
 	if (x < 0)
 	{
 		last_d = (-1 * (x % 10));
-		_putchar (last_d + '0');
+		_putchar(last_d + '0');
 		return (last_d);
 	}
+
 	
 	else
 	{
 		last_d = (x % 10);
-		_putchar (last_d + '0');
+		_putchar(last_d + '0');
 		return (last_d);
 	}
+
 
 }
 
