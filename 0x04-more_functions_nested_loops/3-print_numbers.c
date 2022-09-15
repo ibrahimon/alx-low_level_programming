90e0d2367a57d5b0832613de324b3cfc2fe7c4e2
ibrahimon <ibrahim07helal@gmail.com>
2022-09-16 00:52:19 +0200

    more functions

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,18 +3,17 @@
 
 /**
  * print_numbers - prints numbers from 0 to 9 followed by new line
- *
  * Return: 0 to 9
  */
 
 void print_numbers(void)
 {
 	int i;
-	i = 0;
 
+	i = 0;
 	while (i < 10)
 	{
-		_putchar(i + '0');
+		_putchar(i);
 		i++;
 	}
 
