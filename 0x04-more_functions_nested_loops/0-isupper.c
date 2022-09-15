c1475de9b673fa923a2c2782de5e57e4f95ceef4
Ibrahim Helal <54383232+ibrahimon@users.noreply.github.com>
2022-09-15 21:38:37 +0200

    more functions

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include <stdio>
+
+/**
+ * _isupper - checks for uppercase character
+ * @c: int charachter
+ * Return: 0
+ */
+
+int _isupper(int c)
+{
+	if ((c >= 'A') && (c <= 'Z'))
+	{
+		return (0);
+	}
+
+	else
+	{
+		return (0);
+	}
+}
