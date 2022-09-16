33bdf52783f0849df490a19ab8cfebd3e91bd3cc
ibrahimon <ibrahim07helal@gmail.com>
2022-09-16 02:20:46 +0200

    more functions

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,17 +9,17 @@
 
 void more_numbers(void)
 {
-	int n, i;
+	int i, j;
 
 	for (i = 0; i < 10; i++)
 	{
-		for (n = 0; n < 15; n++)
+		for (j = 0; j < 15; j++)
 		{
-			if (n >= 10)
+			if (j >= 10)
 			{
-				_putchar(n / 10 + '0');
+				_putchar(j / 10 + '0');
 			}
-			_putchar(n % 10 + '0');
+			_putchar(j % 10 + '0');
 		}
 		_putchar('\n');
 	}
