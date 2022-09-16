22c858b7fd4d0e8c52a6ae4d735ae01f6ba72e69
ibrahimon <ibrahim07helal@gmail.com>
2022-09-16 03:37:27 +0200

    more functions

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,29 @@
+#include "main"
+#include <stdio.h>
+
+/**
+ * print_line: draws a stright line 
+ * @n: number of times draw _
+ * Return: line
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar(95);
+		}
+
+		_putchar('\n')
+	}
+
+	else
+	{
+		_putchar('\n');
+	}
+
+}
