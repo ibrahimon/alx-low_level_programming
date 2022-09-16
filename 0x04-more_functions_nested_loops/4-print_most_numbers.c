594602b2903a923c95d2e694e5a9b816f0928054
ibrahimon <ibrahim07helal@gmail.com>
2022-09-16 02:43:00 +0200

    more functions

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -4,12 +4,14 @@
 
 /**
  * print_most_numbers - prints numbers from 0 to 9 
+ *
  * Return: void
  */
 
 void print_most_numbers(void)
 {
 	int i;
+
 	for (i = 0; i < 10; i++)
 	{
 		if (i != 2 && i != 4)
