3d920b9cafe2b81de41a46d43c4424e51b5a2fde
ibrahimon <ibrahim07helal@gmail.com>
2022-09-16 22:40:42 +0200

    more functions

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_triangle - prints a triangle
+ * @size: size of traingle
+ * Return: traingle
+ */
+
+void print_traingle(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+
+	else
+	{
+		int i, j;
+
+		for (i = 1; i <= size; j++)
+		{
+			for (j = i; j < size; i++)
+			{
+				_putchar(' ');
+			}
+			
+			for (j = 1; j <= i; j++)
+			{
+				_putchar('#');
+			}
+			
+			_putchar('\n');
+		}
+	}
+}
