cf73b3e88fa2419c59bb651fa3bb721e2bc824da
ibrahimon <ibrahim07helal@gmail.com>
2022-09-16 02:33:37 +0200

    more functions

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,3 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
 
 void print_numbers(void);
 void print_most_numbers(void);
@@ -14,4 +12,3 @@ int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 
-#endif
