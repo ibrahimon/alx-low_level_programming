9041388d232eb2dc03de4232d845fe7781585041
ibrahimon <ibrahim07helal@gmail.com>
2022-09-19 21:43:36 +0200

    pointerrrrrs

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - returns the lens of a string
+ * @s: the string
+ * Return: 0
+ */
+
+int _strlen(char *s)
+{
+	*s;
+	return (strlen(*s));
+}
