c9eb0ca7f5c3fe626ee4299dd88d38ba33cbcb84
ibrahimon <ibrahim07helal@gmail.com>
2022-09-20 00:43:42 +0200

    pointerrrrrs

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts2 - prints every other character of a string
+ * @str: the pointer to a string
+ * Return: void
+ */
+
+void puts2(char *str);
+{
+	int count = 0;
+
+	while (count > = 0)
+	{
+		if (str[count] == '\0')
+		{
+			_putchar('\n');
+			breaak;
+		}
+
+		if (count % 2 == 0)
+		{
+			_putchar(str[count]);
+		}
+		count++;
+	}
+}
