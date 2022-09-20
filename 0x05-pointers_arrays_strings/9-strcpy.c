e0defe40f646de63854e6edb9a2ed1fbee916f24
ibrahimon <ibrahim07helal@gmail.com>
2022-09-20 02:03:15 +0200

    pointerrrrrs

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -13,14 +13,13 @@ char *_strcpy(char *dest, char *src)
 	int i;
 
 	i = 0;
-	while (src[i] != '\0')
+	while (i >= 0)
 	{
-		dest[i] = src[1];
+		*(dest + i) = *(src + i);
+		if (*(src + i) == '\0')
+			break;
 		i++;
 	}
-	dest[i] = '\0';
-
 	return (dest);
 
 }
-
