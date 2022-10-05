02242ae6d3725f879157d12cf5fe8d073adadd97
ibrahimon <ibrahim07helal@gmail.com>
2022-10-05 20:05:23 +0200

    argc-argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - main
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
