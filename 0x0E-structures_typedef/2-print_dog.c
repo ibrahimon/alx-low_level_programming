aabe035bcdb3043f7654bfa31f36411954713358
Ibrahimon <ibrahim07helal@gmail.com>
2022-10-11 19:42:00 +0200

    deftype

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,9 +1,9 @@
-#include <stdio.h>
 #include "dog.h"
+#include <stdio.h>
 
 /**
  * print_dog - prints a struct dog
- * @d: struct
+ * @d: Struct dog
  * Return: void
  */
 
@@ -12,15 +12,15 @@ void print_dog(struct dog *d)
 	if (d)
 	{
 		if (d->name == 0)
-			print("Name: (nil)\n");
+			printf("Name: (nile)\n");
 		else
 			printf("Name: %s\n", d->name);
 
-	printf("Age: %f\n", d->age);
+		printf("Age: %f\n", d->age);
 
-	if (d->owner == 0)
-		printf("Owner: (nil)\n");
-	else
-		printf("Owner: %s\n", d->owner);
+		if (d->Owner == 0)
+			printf("Owner: (nil)\n");
+		else
+			printf("Owner: %s\n", d->owner);
 	}
 }
