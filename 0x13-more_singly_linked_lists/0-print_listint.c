90707e95d454d76f821edabd09db1473921e599f
Ibrahimon <ibrahim07helal@gmail.com>
2022-10-24 14:43:46 -0700

    more linked list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements pf a listint_t
+ * @h: a pointer to a struct listint_t
+ * Return: size_t
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t size = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		size++;
+		printf("%d\n", h->n);
+		size++;
+	}
+
+	return (size);
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,3 @@
+How to use linked lists
+
+
