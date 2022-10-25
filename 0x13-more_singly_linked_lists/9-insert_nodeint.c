91cbcd9aaf6e96c3b01929366dc2c5bf18228ed6
Ibrahimon <ibrahim07helal@gmail.com>
2022-10-25 16:53:07 -0700

    more lsts likaaan

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,7 +13,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *ptr = *head, *toadd = *head;
 	unsigned int count = 0;
 
-	if (!new || !head)
+	if (!toadd || !head)
 		return (NULL);
 
 	while (ptr)
