863cdcd53396378b5ab70b93178043fd7c7581ca
Ibrahimon <ibrahim07helal@gmail.com>
2022-10-25 16:00:42 -0700

    more lsts likaaan

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - returns the nth node of a listint_t
+ * @head: a pointer to struct
+ * @index: an index of node
+ * Return: listint_t struct
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *ptr = head;
+	unsigned int count = 0;
+
+	if (!ptr || !index)
+		return (NULL);
+	while (ptr != NULL)
+	{
+		if (count == index)
+			return (ptr);
+		count++;
+		ptr = ptr->next;
+	}
+	return (NULL);
+}
