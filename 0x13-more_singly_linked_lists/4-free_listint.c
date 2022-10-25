1cc894c9d82188cbeb9857b32ca6ed508bb80095
Ibrahimon <ibrahim07helal@gmail.com>
2022-10-25 05:00:52 -0700

    more lined listss

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/*
+ * free_listint - frees a listint_t list
+ * @head: a pointer to struct
+ * Return: void
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *node;
+
+	while ((node = head) != NULL)
+	{
+		node = head->next;
+		free(head);
+		head = node;
+	}
+}
