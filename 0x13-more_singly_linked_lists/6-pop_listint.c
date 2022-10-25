bb9db146053bb9318a3ee41cdf14af87be3d81d9
Ibrahimon <ibrahim07helal@gmail.com>
2022-10-25 14:42:55 -0700

    more lists

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * pop_listint - delets the head node of a listint_t
+ * @head: a pointer of pointer of struct
+ * Return: int
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *todel = *head, *nw = *head;
+
+	if (!*head)
+		return (0);
+	if (*head)
+	{
+		todel = *head;
+		nw->next = todel->next;
+		*head = nw;
+	}
+	return ((*head)->n);
+}
