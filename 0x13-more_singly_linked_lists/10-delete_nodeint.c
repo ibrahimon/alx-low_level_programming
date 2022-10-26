64ce09aaada6895d9ccf77952e36e95054b10ecf
Ibrahimon <ibrahim07helal@gmail.com>
2022-10-26 07:31:37 -0700

    more linkests

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -10,26 +10,28 @@
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	listint_t *todel = *head, *nw = *head;
+	unsigned int i = 0;
 
-	if (!(*head) || !head)
+	if (!*head)
 		return (-1);
-	else if (index == 1)
+	else if (index == 0)
 	{
-		*head = todel->next;
+		*head = (*head)->next;
 		free(todel);
-		todel = NULL;
+		return (1);
 	}
 	else
 	{
-		while (index != 1)
+		while (i < index - 1)
 		{
-			nw = todel;
-			todel = todel->next;
-			index--;
+			if (!nw || !(nw->next))
+				return (-1);
+			nw = nw->next;
+			i++;
 		}
+		todel = nw->next;
 		nw->next = todel->next;
 		free(todel);
-		todel = NULL;
 	}
 	return (1);
 }
