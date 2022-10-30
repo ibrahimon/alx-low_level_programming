6fa86128ca6b355d017a00ee595ce293046df069
Ibrahimon <ibrahim07helal@gmail.com>
2022-10-30 09:13:13 -0700

    even more pointers

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - prints the sum of the two diagonals
+ * @a: a pointer to matrix
+ * @size: a size of diagnol
+ * Return: void
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int sum1 = 0, sum2 = 0;
+	int i;
+
+	for (i = 0; i < size; i++)
+	{
+		sum1 += a[i];
+		a += size;
+	}
+
+	a -= size;
+
+	for (i = 0; i < size; i++)
+	{
+		sum2 += a[i];
+		a -= size;
+	}
+	printf("%d, %d\n", sum1, sum2);
+}
+
