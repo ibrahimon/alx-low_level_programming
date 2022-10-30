de29280a0d30177a275fcf990ec93ef29693b005
Ibrahimon <ibrahim07helal@gmail.com>
2022-10-30 08:44:27 -0700

    even more pointers

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_chessboard - prints the chessboard
+ * @a: an array of 8 chars
+ * Return: void
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	while (*a)
+	{
+		for (i = 0; a[i][7]; i++)
+		{
+			for (j = 0; j < 8; j++)
+				_putchar(a[i][j]);
+			_putchar('\n');
+		}
+	}
+}
