ed698b5ace399c8db9acf2a5d3281d35b564f85e
Ibrahimon <ibrahim07helal@gmail.com>
2022-12-20 11:47:06 -0800

    double lists

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -10,6 +10,7 @@
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
 	dlistint_t *new;
+	dlistint_t *new;
 
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
@@ -17,17 +18,18 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 
 	new->n = n;
 	new->next = NULL;
-	if (*head == NULL)
+	h = *head;
+	if (h != NULL)
+	{
+		while (h->next != NULL)
+			h = h->next;
+		h->next = new;
+	}
+	else
 	{
 		*head = new;
-		return (new);
 	}
-	if (*head != NULL)
-	{
-		while ((*head)->next != NULL)
-			*head = (*head)->next;
-		(*head)->next = new;
-	}
+	new->prev = h;
 
 	return (new);
 }
