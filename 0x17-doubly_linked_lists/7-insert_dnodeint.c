82c41b823586652b21a7eedaea668b5e0793922b
Ibrahimon <ibrahim07helal@gmail.com>
2022-12-20 12:34:15 -0800

    double lists

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -32,7 +32,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 					new = add_dnodeint_end(h, n);
 				else
 				{
-					new = malloc(sizeof(dlist_t));
+					new = malloc(sizeof(dlistint_t));
 					if (new != NULL)
 					{
 						new->n = n;
