5c68503d66f18744aa6fe5ea54fb5c26a00e92ed
Ibrahimon <ibrahim07helal@gmail.com>
2022-12-20 13:01:28 -0800

    double lists

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,51 @@
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - delets the node at index
+ * @head: a pointer to pointer to the head
+ * @index: the node index to delete
+ * Return: 1 in success or -1 in failuar
+ */
+
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *h1;
+	dlistint_t *h2;
+	unsigned int i;
+
+	h1 = *head;
+
+	if (h1 == NULL)
+		return (-1);
+	if (h1 != NULL)
+		while (h1->prev != NULL)
+			h1 = h1->prev;
+	i = 0;
+
+	while (h1 != NULL)
+	{
+		if (index < 0)
+			return (-1);
+		if (i == index)
+		{
+			if (i == 0)
+			{
+				*head = h1->next;
+				if (*head != NULL)
+					(*head)->prev = NULL;
+			}
+			else
+			{
+				h2->next = h1->next;
+				if (h1->next != NULL)
+					h1->next->prev = h2;
+			}
+			free(h1);
+			return (1);
+		}
+		h2 = h1;
+		h1 = h1->next;
+		i++;
+	}
+	return (-1);
+}
